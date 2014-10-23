761609fcf6f9201f6e7f004e4b7ed40c3007ddc6
Pablo Novas <pjnovas@gmail.com>
2014-10-23 20:02:15 -0300

    added render example with logo image for both Renderers.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 window.document.title += ' [pac  v' + pac.VERSION + ']';
+
+(function(){
+
+  var ctn = document.getElementById("content");
+
+  var game = pac.create();
+
+  //game.use("renderer", pac.NativeRenderer, { 
+  game.use("renderer", pac.PixiRenderer, { 
+    container: ctn,
+    backgroundColor: "#000000",
+    size: {
+      width: 700, 
+      height: 500 
+    }
+  });
+
+  //START -  hack for test render
+  game.renderer.stage.add({
+    resource: 'assets/images/psychonull_logo_pac.png',
+    position: {
+      x: 100,
+      y: 100
+    },
+    size: {
+      width: 405,
+      height: 135
+    }
+  });
+  //END - 
+
+  game.start();
+
+})();
