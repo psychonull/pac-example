0f6b1e9907acd470b28fbe42df36ee2f1443df33
Matias Arriola <matias.arriola@gmail.com>
2014-10-29 14:20:40 -0300

    updated pac submodule + code for loader usage

--- index.js
+++ index.js
@@ -6,18 +6,40 @@ window.document.title += ' [pac  v' + pac.VERSION + ']';
 
   var game = pac.create();
 
-  //game.use("renderer", pac.NativeRenderer, { 
-  game.use("renderer", pac.PixiRenderer, { 
+  //game.use("renderer", pac.NativeRenderer, {
+  game.use("renderer", pac.PixiRenderer, {
     container: ctn,
     backgroundColor: "#000000",
     size: {
-      width: 700, 
-      height: 500 
+      width: 700,
+      height: 500
     }
   });
 
+  game.use("loader", pac.Loader, {
+    'logo': 'assets/images/psychonull_logo_pac.png'
+  });
+
+  game.loader.on("complete", function(){
+    console.log('logo is: ', game.cache.images.get('logo'));
+    var logoSprite = new pac.Sprite({
+      texture: 'logo',
+      position: {
+        x: 100,
+        y: 100
+      },
+      size: {
+        width: 405,
+        height: 135
+      }
+    });
+    game.renderer.stage.add(logoSprite);
+    game.start();
+  });
+
+  game.loader.load();
   //START -  hack for test render
-  game.renderer.stage.add({
+  /*game.renderer.stage.add({
     resource: 'assets/images/psychonull_logo_pac.png',
     position: {
       x: 100,
@@ -27,8 +49,8 @@ window.document.title += ' [pac  v' + pac.VERSION + ']';
       width: 405,
       height: 135
     }
-  });
-  //END - 
+  });*/
+  //END -
 
   game.start();
 
