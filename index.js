35842f3abcbed1ef35184468a44f0cd7363bd26a
Pablo Novas <pjnovas@gmail.com>
2014-11-02 12:52:22 -0300

    updated example with Actions

--- index.js
+++ index.js
@@ -25,19 +25,58 @@ window.document.title += ' [pac  v' + pac.VERSION + ']';
     size: { width: 500, height: 400 }
   });
 
-  var LogoPrefab = pac.Sprite.extend({
-    texture: 'logo',
-    update: function(dt){
-      this.position.x += 50 * dt;
+  // ACTION: Horizontal movement of an object 
+  var MoveH = pac.Action.extend({
 
-      if (this.position.x + this.size.width > 700){
-        this.position.x = 0;
+    vel: 50,
+    dir: 1,
+    bounds: { min: 0, max: 700 },
+
+    init: function(options){
+      this.vel = (options && options.vel) || this.vel;
+      this.bounds = (options && options.bounds) || this.bounds;
+    },
+
+    update: function(dt) {
+      var obj = this.actionList.owner;
+
+      obj.position.x += this.vel * dt * this.dir;
+
+      if (obj.position.x + obj.size.width > this.bounds.max || 
+        obj.position.x <= this.bounds.min ) {
+
+          this.isFinished = true;
       }
+    }
+
+  });
+
+  var MoveRight = MoveH.extend({
+    onEnd: function(){
+      this.insertInFrontOfMe(new MoveLeft({ 
+        vel: this.vel, 
+        bounds: this.bounds 
+      }));
     }
   });
 
+  var MoveLeft = MoveH.extend({
+    dir: -1,
+    onEnd: function(){
+      this.insertInFrontOfMe(new MoveRight({ 
+        vel: this.vel, 
+        bounds: this.bounds 
+      }));
+    }
+  });
+
+
+  var LogoPrefab = pac.Sprite.extend({
+    texture: 'logo'
+  });
+
   var logoBig = new LogoPrefab({
-    texture: 'logo',
+    actions: [ new MoveRight({ vel: 500 }) ],
     position: {
       x: 100,
       y: 100
@@ -49,7 +88,7 @@ window.document.title += ' [pac  v' + pac.VERSION + ']';
   });
 
   var logoSmall = new LogoPrefab({
-    texture: 'logo',
+    actions: [ new MoveLeft({ vel: 200 }) ],
     position: {
       x: 400,
       y: 400
@@ -60,8 +99,21 @@ window.document.title += ' [pac  v' + pac.VERSION + ']';
     }
   });
 
+  var logoSmall2 = new LogoPrefab({
+    actions: [ new MoveRight({ vel: 300, bounds: { min: 100, max: 400 } }) ],
+    position: {
+      x: 100,
+      y: 300
+    },
+    size: {
+      width: 202,
+      height: 67
+    }
+  });
+
   scene.addObject(logoBig);
   scene.addObject(logoSmall);
+  scene.addObject(logoSmall2);
 
   game.scenes.add(scene);
 
